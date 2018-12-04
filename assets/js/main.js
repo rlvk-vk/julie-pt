fff2957485161435fe82a5f8be339292f8a8eea7
Robin <rlvk94@gmail.com>
2018-12-04 20:38:04 +0100

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-"use strict";$(document).ready(function(){menuHandler()});var menuHandler=function(){$("#toggle-menu").on("click",function(){$("#overlay").fadeToggle(),$("header").toggleClass("toggled"),console.log($(this))})};
+"use strict";$(document).ready(function(){menuHandler()});var menuHandler=function(){$("#toggle-menu").on("click",function(){$("#overlay").fadeToggle(),$("header").toggleClass("toggled"),console.log($(this).next().find("li"))})};
