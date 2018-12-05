ba86b927e832d14d0bacd69179c9263855a6ea05
Robin <rlvk94@gmail.com>
2018-12-05 17:49:31 +0100

    add font

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
 		<title><?php echo $PAGE_TITLE; ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" media="screen" href="/assets/css/main.css" />
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,700" rel="stylesheet">
 	</head>
 	<body>
 		<header>
