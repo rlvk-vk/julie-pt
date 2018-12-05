16fdf3f47f143c8e170c404759130933ccf457f0
Robin <rlvk94@gmail.com>
2018-12-05 18:19:13 +0100

    add some footer content

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,19 @@
+		<footer>
+			<a href="/">Footer Logo</a>
+
+			<section id="f-c">
+				<h3>Kontakt</h3>
+				<p>Julie Bechmann Jensen</p>
+				<p>julie@vkmedia.dk</p>
+				<p>12 34 56 78</p>
+			</section>
+
+			<section id="sm">
+				<a href="#">Facebook</a>
+				<a href="#">Instagram</a>
+			</section>
+		</footer>
+
 		<section id="overlay"></section>
 		<script src="/assets/js/jquery-3.3.1.min.js"></script>
 		<script src="/assets/js/main.js"></script>
