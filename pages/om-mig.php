12ea3328335ff49d310f08c75dc88e3399e6ceee
Robin <rlvk94@gmail.com>
2018-12-05 21:02:32 +0100

    add content to about me page

--- pages/om-mig.php
+++ pages/om-mig.php
@@ -2,6 +2,95 @@
 $PAGE_TITLE = 'Om mig | Julie Personlig Træner';
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
 
-<h1>Om mig</h1>
+<main>
+	<h1 id="p-t">Om mig</h1>
+
+	<div class="i ih-2" style="background-image: url('/assets/images/om-mig.jpg');">
+		<div class="if"></div>
+	</div>
+
+	<div class="q">“For mig handler fitness ikke kun om at træne i fitnesscenteret, det handler også om indre lykke og velvære.”</div>
+
+	<div class="t bg-d">
+		<div>Mit navn er Julie Bechmann Jensen og jeg arbejder som coach, kostvejleder og personlig træner. Min arbejde og passion ligger i at kunne vejlede folk, give dem værktøjer til et vedvarende mål, samt hjælpe dem i den helt rigtige retning i forhold til dMit navn er Julie Bechmann Jensen og jeg arbejder som coach, kostvejleder og personlig træner. Min arbejde og passion ligger i at kunne vejlede folk, give dem værktøjer til et vedvarende mål, samt hjælpe dem i den helt rigtige retning i forhold til det de ønsker. For mig er det en drivfaktor at der skal være plads til selvudvikling og at mine klienter kan være med til at udvikle deres træningsprogrammer, kost og mål. Det er vigtigt for mig at det er noget vi gør i fællesskab så mine produkter og det jeg tilbyder passer netop til min klient.</div>
+	</div>
+
+	<div class="twi">
+		<div class="i" style="background-image: url('/assets/images/elsker-at-videreformidle.jpg');">
+			<div class="if"></div>
+			<h2>Jeg elsker at videreformidle den viden om kost og træning som jeg har erfaret virker</h2>
+		</div>
+		<div class="c">
+			<div class="b">Jeg valgt at specialisere mig indenfor vegansk kost, både i forhold til træning men også på generel basis. For mig er det helt klart en af de ting som jeg vægter utrolig højt, da jeg selv blev veganer i 2017. Så hvis du er nysgerrig på en mere plantebaseret kost i forbindelse med din træning, er du kommet det helt rigtige sted hen. Men hvis du derimod ikke har mod på at smide de animalske produkter helt endnu, skal du heller ikke være bleg for at kontakte mig, da jeg kan hjælpe dig med en sundere livsstil og få et bedre forhold til din kost hvad end du er kødspiser, spiser plantebaseret eller har et ønske om at ændre din kost i forbindelse med træning.</div>
+			<button class="cta">Skriv til mig</button>
+		</div>
+	</div>
+
+	<div class="i ih-2" style="background-image: url('/assets/images/om-mig.jpg');">
+		<div class="if"></div>
+	</div>
+
+	<div class="q">“For mig er fitness en stor del af mit hverdagsliv. Men fitness betyder ikke at have store muskler. Det betyder at være aktiv, hurtig og fleksibel. Det kan defineres på mange forskellige måder.”</div>
+
+	<div class="t bg-d">
+		<div>Jeg tror ikke på at et træningsprogram på 8 uger med en meget stram kostplan eller diæt er vejen frem for alle, så derfor er det vigtigt for mig at tage højde for mine klienters mål og ønsker i overensstemmelse hvad de har lyst til at prøve kræfter med og kan finde motivation og glæde ved.<br /><br />For mig er det vigtigt at kunne hjælpe folk og være med til at udarbejde og finde en løsning som passer til lige netop dig.<br /><br />Skal jeg være en del af dine personlige rejse og hjælpe dig på vej til dine mål eller finde lyst og motivationen til din træning så kontakt mig på juliebech95@gmail.com eller skriv til mig her på siden.</div>
+	</div>
+
+	<div class="twi">
+		<div class="i" style="background-image: url('/assets/images/elsker-at-videreformidle.jpg');">
+			<div class="if"></div>
+			<h2>Min historie</h2>
+		</div>
+		<div class="c">
+			<div class="b">Da jeg var barn begyndte jeg til gymnastik og jeg elskede det. Først var jeg springgymnast og derefter blev jeg rytmegymnast hvor dans og bevægelse fyldte meget. Jeg fortsætter til jeg var 16 år og begyndte på gymnasiet hvor mit forhold til mad, motion og træning styrtdykkede markant. Jeg var nok den eneste, for der skete en masse ting i mit liv på det tidspunkt så det var ikke de ting som jeg fokuserede særlig meget på. Jeg spiste og drak hvad der passede mig, for jeg har aldrig haft problemer med min vægt. Har altid været høj og tynd, så det passede mig fint bare at spise hvad jeg magtede. Men hold nu op hvor fik det konsekvenser, specielt for min mave men også min hud.</div>
+		</div>
+	</div>
+
+	<div class="i ih-2" style="background-image: url('/assets/images/klar-til-loeb.jpg');">
+		<div class="if"></div>
+	</div>
+
+	<div class="t bg-d">
+		<div>Jeg døjede med maveproblemer fra jeg var 17 år til jeg blev 22 år, hvor jeg begyndte en del af de år til at finde ud af hvad i alverden der var galt med mig. Først og fremmest fandt jeg ud af at min krop reagerede utrolig dårligt på alt hvad der hed mælkeprodukter og det blev virkelig en udfordring, for alt hvad jeg tidligere bare havde puttet i munden uden at tænke over det, indhold cirka 70 procent af det, en eller anden form for mælk. Jeg blev langsomt bedre og bedre til det og fik det også markant bedre i min mave og krop efter jeg cuttede det fra min kost.</div>
+	</div>
+
+	<div class="twi">
+		<div class="i" style="background-image: url('/assets/images/mit-forhold-til-traening.jpg');">
+			<div class="if"></div>
+			<h2 class="q">“Mit forhold til træning og kost har ændret sig meget hen over de sidste mange år af mit liv”</h2>
+		</div>
+		<div class="c">
+			<div class="b">Derefter begyndte min interesse i træning. Flere og flere jeg kendte var begyndt og træne og da jeg havde droppet min gymnastik, følte jeg et behov for at lave et eller andet fysisk. Jeg har altid været utrolig dårlig til konditionstræning, så det faldt mig ikke naturlig at det skulle være noget i den retning. Derfor begyndte jeg at styrketræne. Jeg var lige blevet færdig med gymnasiet og skulle til at læse videre. Jeg trænede cirka 3 gange om ugen, men jeg havde ikke den fjerneste ide om hvad jeg lavede. Det er nok normalt for de fleste nybegynder, men jeg havde ikke sat mig ind i noget som helst. Jeg havde ingen mål, ingen erfaring, ingen jeg kunne spørger til råds og jeg ville ikke kontakte en personlig træner med mere erfaring.</div>
+		</div>
+	</div>
+
+	<div class="i ih-2" style="background-image: url('/assets/images/vegansk-livsstil.jpg');">
+		<div class="if"></div>
+		<h2 class="q">“Jeg er stor tilhænger af den platebaserede kost, både i forhold til sundhed og helbredsmæssige fordele.”</h2>
+	</div>
+
+	<div class="t bg-d">
+		<div>I 2017 stødte jeg ind i den veganske livsstil og dens verden. Min kæreste som i forvejen levede vegansk præsenteret mig for den, da jeg i forvejen havde skåret meget ned på min indtag af kød, da jeg havde en ide om at min mave ikke var særlig glad for det. Derfor begyndte jeg at blive nysgerrig på den plantebaseret livsstil og august 2017 opgav jeg kødet for alvor. Det var en hel ny verden for mig og hold nu op hvor lærte jeg meget. Min krop og mit energiniveau blev pludselig noget helt andet og min mave har aldrig været bedre. Jeg undersøgte efterhånden mere og mere og blev klar over alle de sundhedsmæssig fordele ved vegansk kost.</div>
+	</div>
+
+	<div class="twi">
+		<div class="i" style="background-image: url('/assets/images/traening-i-min-hverdag.jpg');">
+			<div class="if"></div>
+			<h2 class="q">“Træning i min hverdag har givet mig mere overskud og jeg fandt lysten til at bevæge mig”</h2>
+		</div>
+		<div class="c">
+			<div class="b">Træning i min hverdag har givet mig meget mere overskud og jeg har fundet lysten til at træne. Det har givet mig lyst til at udfordre og presse mig selv og være en i konstant udvikling, uanset om jeg træner hårdt og virkelig har fokus på min kost men også når jeg slapper mere af og spiser sundt men hvad jeg har lyst til.</div>
+		</div>
+	</div>
+
+	<div class="i ih-2" style="background-image: url('/assets/images/erfaring-med-traening.jpg');">
+		<div class="if"></div>
+		<h2 class="q">“Jeg begyndte at få mere erfaring men først efter flere års træning fandt jeg ud af hvad der virkede for mig”</h2>
+	</div>
+
+	<div class="t bg-d">
+		<div>Igennem min træningsrejse har jeg prøvet det meste. Jeg har brugt uendelig lang tid på et løbebånd og være fuld fokuseret på cardio, da jeg var overbevist om at det var vejen frem for at tabe sig og blive mere tonet. Jeg har fulgt en masse videoer og hjemmetræningsprogrammer med tusindvis af øvelser og repetitioner og jeg har lavet alle mulige former for vægtløftningsøvelser og variationer uden i virkeligheden at vide hvad det egentlig var jeg gerne ville og hvordan jeg i det hele taget skulle træne.</div>
+	</div>
+</main>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
