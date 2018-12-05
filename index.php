0e2725b180cbe600ae9bbe339b4ec3a3ca620e33
Robin <rlvk94@gmail.com>
2018-12-05 07:37:55 +0100

    add template styling + content on landingpage

--- index.php
+++ index.php
@@ -2,6 +2,71 @@
 $PAGE_TITLE = 'Julie | Personlig Træner';
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
 
-<h1>Forside</h1>
+<main>
+	<div id="hero">
+		<div class="if"></div>
+		<div class="c">
+			<h1>Personlig træner, coach og specialist i vegansk livsstil</h1>
+		</div>
+	</div>
+
+	<div class="t bg-g">
+		<div>Du kan altid forbedre dig, hvis du bliver ved med at træne og ligemeget hvor god du bliver, kan du altid blive bedre, hvilket er den fantastiske del af det. Hvis du samtidig har noget du gerne vil bevise overfor dig selv og andre, er der intet bedre end en udfordring.</div>
+	</div>
+
+	<div class="t bg-d">
+		<h3>Hold din krop</h3>
+		<h2>Fit & stærk</h2>
+		<div>Som personlig træner på fokus på en plantebaseret livsstil, ville jeg kunne give dig en unik løsning som passer til lige netop dig. Jeg  tror ikke på at én løsning passer til alle, men at vi sammen kan tilpasse og planlægge et individuelt forløb så du kan nå dine mål.</div>
+	</div>
+
+	<div class="twi">
+		<div class="i">
+			<div class="if"></div>
+			<h3>Nyd en hverdag med træning som du elsker</h3>
+		</div>
+		<div class="c">
+			<div class="b">Med personlig træning er det dine mål  og ønsker til træning som er i fokus og jeg møder dig der hvor du er.</div>
+			<button class="cta">Læs mere</button>
+		</div>
+	</div>
+
+	<div class="iwt">
+		<div class="if"></div>
+		<div class="c">
+			<h2>Det rette mindset og fuld fokus på dine mål</h2>
+			<div class="b">Jeg tror på at man sagtens kan opbygge muskler og styrke udelukkede på en plantebaseret kost. Det handler om at være opmærksom på hvad man indtager og det passer til ens personlige mål<br /><br />Vil du høre mere om hvad jeg kan gøre for dig?</div>
+			<button class="cta">Personlig træning</button>
+		</div>
+	</div>
+
+	<div class="t bg-d">
+		<h3>Fleksibelt træningsprogram</h3>
+		<h2>Dig og din hverdag</h2>
+		<div>Med et online forløb hos mig, kan du føle dig lige så sikker som ved personlig træning. Som træner vil jeg og min klient arbejde tæt sammen blandt andet via. mail, telefon eller skype så du kan føle dig tryg under hele træningsforløbet.</div>
+	</div>
+
+	<div class="twi">
+		<div class="i">
+			<div class="if"></div>
+			<h3>Ligemeget hvor du er</h3>
+		</div>
+		<div class="c">
+			<div class="b">Uanset hvor du befinder dig i Danmark eller andre dele af verden kan du få en personlig træner, selv hvis du ikke har muligheden for at komme hvor jeg er.</div>
+			<button class="cta">Online forløb</button>
+		</div>
+	</div>
+
+	<div class="iwt">
+		<div class="if"></div>
+		<div class="c">
+			<h2>E-bøger og guides til dig</h2>
+			<div class="b">Inspiration til at kickstarte træningsforløb og gode råd til den veganske livsstil?</div>
+			<button class="cta">Læs mere</button>
+		</div>
+	</div>
+
+	<!-- LATEST BLOGS SHOULD GO HERE AS SOON AS BLOG IS READY -->
+</main>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
