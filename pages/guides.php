99544dd53e37d60dd59438eeb065dc41266dd8f4
Robin <rlvk94@gmail.com>
2018-12-09 17:41:34 +0100

    add content to 2 pages

--- pages/guides.php
+++ pages/guides.php
@@ -4,6 +4,26 @@ include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
 
 <main>
 	<h1 id="p-t">Guides</h1>
+
+	<div class="i ih-2" style="background-image: url('/assets/images/gode-traeningsprogrammer.jpg');">
+		<div class="if"></div>
+	</div>
+
+	<div class="t bg-d">
+		<div>Mangler du inspiration til din træning eller går du rundt med tanker at om at spise mere plantebaseret men ved ikk helt hvor du skal starte? Så kig med her. Her får du inspiration til kickstarte din træning med aktiviteter som du synes er fantastiske at bruge tid på, opskrifter, gode vaner og råd til den veganske livsstil og hvordan det kan gøres meget nemmere og meget meget mere!</div>
+	</div>
+
+	<div class="twi">
+		<div class="i" style="background-image: url('/assets/images/personlig-traening.jpg');">
+			<div class="if"></div>
+			<h2 class="ta-r">Kickstart din træning</h2>
+		</div>
+		<div class="c">
+			<div class="b">Guide til at komme i gang med træning <br />Tips og gode vaner til at vedligeholde din træning<br />Ideer til at finde netop dig retning og træningsmetode</div>
+
+			<button class="cta">Læs mere</button>
+		</div>
+	</div>
 </main>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
