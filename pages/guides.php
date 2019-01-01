104bfad66609a970abe4c67e824bf0689b5b9daf
Robin Louis van Komen <rlvk94@gmail.com>
2019-01-01 16:19:51 +0100

    web styling

--- pages/guides.php
+++ pages/guides.php
@@ -5,23 +5,25 @@ include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
 <main>
 	<h1 id="p-t">Guides</h1>
 
-	<div class="i ih-2" style="background-image: url('/assets/images/guides.jpg');">
-		<div class="if"></div>
-	</div>
-
-	<div class="t bg-d">
-		<div>Mangler du inspiration til din træning eller går du rundt med tanker at om at spise mere plantebaseret men ved ikk helt hvor du skal starte? Så kig med her. Her får du inspiration til kickstarte din træning med aktiviteter som du synes er fantastiske at bruge tid på, opskrifter, gode vaner og råd til den veganske livsstil og hvordan det kan gøres meget nemmere og meget meget mere!</div>
-	</div>
-
-	<div class="twi">
-		<div class="i" style="background-image: url('/assets/images/kickstart-din-traening.jpg');">
+	<div class="d-f-lg fw-w-lg jc-sa-lg desktop-container">
+		<div class="i ih-2 w-6-lg o-1-lg" style="background-image: url('/assets/images/guides.jpg');">
 			<div class="if"></div>
-			<h2 class="ta-c">Kickstart din træning</h2>
 		</div>
-		<div class="c">
-			<div class="mb-2">Guide til at komme i gang med træning <br />Tips og gode vaner til at vedligeholde din træning<br />Ideer til at finde netop dig retning og træningsmetode</div>
 
-			<button class="cta center">Læs mere</button>
+		<div class="t bg-d w-6-lg o-2-lg">
+			<div>Mangler du inspiration til din træning eller går du rundt med tanker at om at spise mere plantebaseret men ved ikk helt hvor du skal starte? Så kig med her. Her får du inspiration til kickstarte din træning med aktiviteter som du synes er fantastiske at bruge tid på, opskrifter, gode vaner og råd til den veganske livsstil og hvordan det kan gøres meget nemmere og meget meget mere!</div>
+		</div>
+
+		<div class="twi w-4-lg o-3-lg">
+			<div class="i" style="background-image: url('/assets/images/kickstart-din-traening.jpg');">
+				<div class="if"></div>
+				<h2 class="ta-c">Kickstart din træning</h2>
+			</div>
+			<div class="c">
+				<div class="mb-2">Guide til at komme i gang med træning <br />Tips og gode vaner til at vedligeholde din træning<br />Ideer til at finde netop dig retning og træningsmetode</div>
+
+				<button class="cta center">Læs mere</button>
+			</div>
 		</div>
 	</div>
 </main>
