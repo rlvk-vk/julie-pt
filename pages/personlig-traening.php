84176a4cf6c4afa99b97c3e876244332c5cb0027
Robin Louis van Komen <rlvk94@gmail.com>
2019-01-01 16:05:53 +0100

    web styling

--- pages/personlig-traening.php
+++ pages/personlig-traening.php
@@ -5,33 +5,35 @@ include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
 <main>
 	<h1 id="p-t">Personlig træning</h1>
 
-	<div class="i ih-2" style="background-image: url('/assets/images/personlig-traening.jpg');">
-		<div class="if"></div>
-	</div>
-
-	<div class="t bg-d">
-		<div class="mb-2">Alle mennesker er forskellige og det gør sig også gældende når der er tale om træning og kost. Derfor kan et personlig træningsforløb være lige noget for dig. Ønsker du at komme i gang med din træning, men har svært ved at finde overskuddet, motivationen eller ved måske slet ikke hvor du skal starte?<br />Med et personlig træningsforløb hos mig kan jeg være lige netop det du har brug for, for at få et skub i den rigtige retning.<br />Personlig træning er for alle, lige fra studerende til pensionist, også selvom dine mål er vidt forskellige.<br />Et personligt træningsforløb er for dig som gerne vil presses til at nå dine mål. Jeg kan være med til at presse dig  til det yderste.</div>
-		<button class="cta center">Se anbefalinger</button>
-	</div>
-
-	<div class="twi">
-		<div class="i" style="background-image: url('/assets/images/udnyt-dit-potentiale.jpg');">
+	<div class="d-f-lg desktop-container">
+		<div class="i ih-2 w-6-lg o-1-lg" style="background-image: url('/assets/images/personlig-traening.jpg');">
 			<div class="if"></div>
-			<h2 class="ta-r os-3">Udnyt dit potentiale 100 procent hver eneste gang du træner</h2>
 		</div>
-		<div class="c">
-			<div class="b">Det kan være svært alene at udnytte sit potentiale 100 procent. Som personlig træner vil jeg kunne presse dig tilpas meget så du får det maksimale ud af din træning, samtidig med at du undgå skader og overbelastninger og jeg vil kunne evaluere din fremskridt så du hele tiden har en ide at du hele tiden udvikler dig, i den rigtige retning og kan give et boost i forhold til motivationen, når du ved at der sker fremskridt og du rykker dig.<br />Som din personlige træner vil jeg både at presse dig når der er brug for det, give dig gode råd, motivere dig til at nå dit mål og støtte dig hele vejen igennem, uanset som du ønsker et vægttab, en bedre form, opbygge mere muskelmasse eller få en bedre teknik. Jeg vil give dig professionel vejledning, uanset om du har brug for 4 træninger sammen med mig i løbet af en uge eller har brug for mere frie rammer, og ind imellem har brug for et skulderklap på vejen. Jeg kan hjælpe dig med at konkretisere dine mål og gøre dem realistiske.<br />Derfor gør jeg en ære i at skræddersy netop det helt rigtige forløb til, så det passer på lige netop og at du får den helt rigtige vejledning og som personlig træner vil jeg sammen med dig sætte nogle træningsmål, i overensstemmelse med dine ønsker, samt en kost som virker overskuelig for dig uden restriktioner  og forbudte fødevarer, men på baggrund af mine anbefalinger.</div>
+
+		<div class="t bg-d w-6-lg o-2-lg">
+			<div class="mb-2 ta-l-lg fs-2-lg">Alle mennesker er forskellige og det gør sig også gældende når der er tale om træning og kost. Derfor kan et personlig træningsforløb være lige noget for dig. Ønsker du at komme i gang med din træning, men har svært ved at finde overskuddet, motivationen eller ved måske slet ikke hvor du skal starte?<br />Med et personlig træningsforløb hos mig kan jeg være lige netop det du har brug for, for at få et skub i den rigtige retning.<br />Personlig træning er for alle, lige fra studerende til pensionist, også selvom dine mål er vidt forskellige.<br />Et personligt træningsforløb er for dig som gerne vil presses til at nå dine mål. Jeg kan være med til at presse dig  til det yderste.</div>
+			<button class="cta center">Se anbefalinger</button>
 		</div>
-	</div>
 
-	<div class="i ih-2" style="background-image: url('/assets/images/gode-traeningsprogrammer.jpg');">
-		<div class="if"></div>
-		<h2 class="ta-l">Gode træningsprogrammer til dig der stadigvæk er lidt i tvivl</h2>
-	</div>
+		<div class="twi w-4-lg o-3-lg">
+			<div class="i" style="background-image: url('/assets/images/udnyt-dit-potentiale.jpg');">
+				<div class="if"></div>
+				<h2 class="ta-r os-3">Udnyt dit potentiale 100 procent hver eneste gang du træner</h2>
+			</div>
+			<div class="c">
+				<div class="b">Det kan være svært alene at udnytte sit potentiale 100 procent. Som personlig træner vil jeg kunne presse dig tilpas meget så du får det maksimale ud af din træning, samtidig med at du undgå skader og overbelastninger og jeg vil kunne evaluere din fremskridt så du hele tiden har en ide at du hele tiden udvikler dig, i den rigtige retning og kan give et boost i forhold til motivationen, når du ved at der sker fremskridt og du rykker dig.<br />Som din personlige træner vil jeg både at presse dig når der er brug for det, give dig gode råd, motivere dig til at nå dit mål og støtte dig hele vejen igennem, uanset som du ønsker et vægttab, en bedre form, opbygge mere muskelmasse eller få en bedre teknik. Jeg vil give dig professionel vejledning, uanset om du har brug for 4 træninger sammen med mig i løbet af en uge eller har brug for mere frie rammer, og ind imellem har brug for et skulderklap på vejen. Jeg kan hjælpe dig med at konkretisere dine mål og gøre dem realistiske.<br />Derfor gør jeg en ære i at skræddersy netop det helt rigtige forløb til, så det passer på lige netop og at du får den helt rigtige vejledning og som personlig træner vil jeg sammen med dig sætte nogle træningsmål, i overensstemmelse med dine ønsker, samt en kost som virker overskuelig for dig uden restriktioner  og forbudte fødevarer, men på baggrund af mine anbefalinger.</div>
+			</div>
+		</div>
 
-	<div class="t bg-d">
-		<div class="mb-2">Det kan være svært at vide hvad man ønsker når man skal i gang med at træne og derfor vil jeg give dig mine anbefalinger til mit personlige træningsforløb, der indeholder træningsprogrammer af en uges varighed, med et forløb på 8-10 uger tilpasset dine ønsker. En kostplan udarbejdet alene så den er tilpasset dine behov og mål. Kropsanalyse som indeholder måling af fedtprocent, cm, samt billeder af hele din process hvis du ønsker dette. Der er som minimum en ugentlig opfølgning på dig og din udvikling, 2 timers træning om ugen i selskab med mig eller efter dit behov og du har mulighed for at kontakte mig over mail eller telefon under hele forløbet.<br /><br/>Prisen på et “to gange om ugen” træningsforløb er 3000 ,-  pr. måned med 8 timers personlig træning. 375,- pr. time<br /><br />Kontakt mig endelig på mail eller her på hjemmesiden for at booke et træningsforløb eller for at høre mere om hvad jeg kan gøre for lige netop dig. </div>
-		<button class="cta center">Skriv til mig</button>
+		<div class="i ih-2 w-6-lg o-4-lg" style="background-image: url('/assets/images/gode-traeningsprogrammer.jpg');">
+			<div class="if"></div>
+			<h2 class="ta-l">Gode træningsprogrammer til dig der stadigvæk er lidt i tvivl</h2>
+		</div>
+
+		<div class="t bg-d w-6-lg o-5-lg">
+			<div class="mb-2 ta-l-lg fs-2-lg">Det kan være svært at vide hvad man ønsker når man skal i gang med at træne og derfor vil jeg give dig mine anbefalinger til mit personlige træningsforløb, der indeholder træningsprogrammer af en uges varighed, med et forløb på 8-10 uger tilpasset dine ønsker. En kostplan udarbejdet alene så den er tilpasset dine behov og mål. Kropsanalyse som indeholder måling af fedtprocent, cm, samt billeder af hele din process hvis du ønsker dette. Der er som minimum en ugentlig opfølgning på dig og din udvikling, 2 timers træning om ugen i selskab med mig eller efter dit behov og du har mulighed for at kontakte mig over mail eller telefon under hele forløbet.<br /><br/>Prisen på et “to gange om ugen” træningsforløb er 3000 ,-  pr. måned med 8 timers personlig træning. 375,- pr. time<br /><br />Kontakt mig endelig på mail eller her på hjemmesiden for at booke et træningsforløb eller for at høre mere om hvad jeg kan gøre for lige netop dig. </div>
+			<button class="cta center">Skriv til mig</button>
+		</div>
 	</div>
 </main>
 
