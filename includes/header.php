dc5b69efe8986cf7947f328f9f34212210c48131
Robin Louis van Komen <robin@vkmedia.dk>
2019-05-02 20:14:59 +0200

    change gtm key

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-PCJNTBF');</script>
+		})(window,document,'script','dataLayer','GTM-KL82XHM');</script>
 		<!-- End Google Tag Manager -->
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,7 +27,7 @@
 	</head>
 	<body <?php if(isset($_SERVER['REQUEST_URI'])){echo $_SERVER['REQUEST_URI'] === '/' ? 'class="landing-page"' : null;} ?>>
 		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCJNTBF"
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KL82XHM"
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 		
